91ade44309e1573a232133ff664582071e1a4b11
vsleios <billtoubanakis@yahoo.gr>
2026-07-04 18:06:19 +0300

    remove favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -109,7 +109,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "canonical", href: "https://www.greekaiolympiad.gr" },
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       {
         rel: "preconnect",
